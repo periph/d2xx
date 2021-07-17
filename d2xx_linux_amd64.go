997687f219ed038c629933b7bec0a9e4547d91a5
Marc-Antoine Ruel <maruel@gmail.com>
2021-07-17 17:57:13 -0400

    d2xx: upgrade to v1.4.24 on linux
    macOS (darwin) will have to be done separately because I need access to
    a mac, the driver is provided as a DMG file.

--- d2xx_linux_amd64.go
+++ d2xx_linux_amd64.go
@@ -8,6 +8,6 @@
 package d2xx
 
 /*
-#cgo LDFLAGS: ${SRCDIR}/third_party/libftd2xx_linux_amd64_v1.4.6.a
+#cgo LDFLAGS: ${SRCDIR}/third_party/libftd2xx_linux_amd64_v1.4.24.a
 */
 import "C"
--- d2xx_linux_arm64.go
+++ d2xx_linux_arm64.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Periph Authors. All rights reserved.
+// Copyright 2021 The Periph Authors. All rights reserved.
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
@@ -7,10 +7,7 @@
 
 package d2xx
 
-// TODO(maruel): https://github.com/golang/go/issues/7211 would help target the
-// optimal ARM architecture.
-
 /*
-#cgo LDFLAGS: ${SRCDIR}/third_party/libftd2xx_linux_arm64_v1.4.8.a
+#cgo LDFLAGS: ${SRCDIR}/third_party/libftd2xx_linux_arm64_v1.4.24.a
 */
 import "C"
